ff175c636a3e6986f7cb0825df93d62579636944
G Lecluse <godefroy.lecluse@eleve.isep.fr>
2025-04-16 12:09:34 +0200

    Debut travail

--- src/main/java/org/isep/cleancode/calculator/Calculator.java
+++ src/main/java/org/isep/cleancode/calculator/Calculator.java
@@ -1,8 +1,46 @@
+// A rendre compte.florian@gmail.com
+
 package org.isep.cleancode.calculator;
 
 public class Calculator {
     public double evaluateMathExpression(String expression) {
-        
         return 0;
+    }
+
+    private List rewriteExpression(String expression) {
+        expression = expression.replace(" ","");
+        List<String> expressionList = new List();
+        StringBuilder currentNumber = new StringBuilder();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+
+            if (Character.isDigit(ch) || ch == '.') {
+                currentNumber.append(ch);
+            } else if (isOperator(ch)) {
+                expressionList.add(currentNumber.toString());
+                currentNumber.setLength(0);
+                }
+                expressionList.add(ch);
+            }
+        return expressionList
+    }
+
+    private evaluateExpressionList(List expressionList) {
+        double resultat = 0
+        for (int i = 0; i<expressionList.length(); i++) {
+            char ch = expressionList.charAt(i)
+
+            if (isOperator(ch)) {
+                switch(ch) {
+                    case '+':
+                        resultat = resultat + 
+                }
+            }
+        }
+    } 
+        
+    private static boolean isOperator(char ch) {
+    return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '(' || ch == ')';
     }
 }
