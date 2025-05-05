c524e52a986a23f412edb81c020c3edfd13d19bc
G Lecluse <godefroy.lecluse@eleve.isep.fr>
2025-05-05 16:48:26 +0200

    passage tests

--- src/main/java/org/isep/cleancode/calculator/Calculator.java
+++ src/main/java/org/isep/cleancode/calculator/Calculator.java
@@ -1,15 +1,16 @@
-// A rendre compte.florian@gmail.com
-
 package org.isep.cleancode.calculator;
+
+import java.util.ArrayList;
 
 public class Calculator {
     public double evaluateMathExpression(String expression) {
-        return 0;
+        ArrayList<String> expressionList = rewriteExpression(expression);
+        return evaluateExpressionList(expressionList);
     }
 
-    private List rewriteExpression(String expression) {
-        expression = expression.replace(" ","");
-        List<String> expressionList = new List();
+    private ArrayList<String> rewriteExpression(String expression) {
+        expression = expression.replace(" ", "");
+        ArrayList<String> expressionList = new ArrayList<>();
         StringBuilder currentNumber = new StringBuilder();
 
         for (int i = 0; i < expression.length(); i++) {
@@ -18,29 +19,65 @@ public class Calculator {
             if (Character.isDigit(ch) || ch == '.') {
                 currentNumber.append(ch);
             } else if (isOperator(ch)) {
-                expressionList.add(currentNumber.toString());
-                currentNumber.setLength(0);
+                if (currentNumber.length() > 0) {
+                    expressionList.add(currentNumber.toString());
+                    currentNumber.setLength(0);
                 }
-                expressionList.add(ch);
-            }
-        return expressionList
-    }
-
-    private evaluateExpressionList(List expressionList) {
-        double resultat = 0
-        for (int i = 0; i<expressionList.length(); i++) {
-            char ch = expressionList.charAt(i)
-
-            if (isOperator(ch)) {
-                switch(ch) {
-                    case '+':
-                        resultat = resultat + 
-                }
+                expressionList.add(String.valueOf(ch));
             }
         }
-    } 
-        
+
+        if (currentNumber.length() > 0) {
+            expressionList.add(currentNumber.toString());
+        }
+
+        return expressionList;
+    }
+
+    private double evaluateExpressionList(ArrayList<String> expressionList) {
+        return evaluate(expressionList, 0, expressionList.size() - 1);
+    }
+
+    private double evaluate(ArrayList<String> expressionList, int start, int end) {
+        if (start == end) {
+            return Double.parseDouble(expressionList.get(start));
+        }
+
+        int operatorIndex = findOperatorIndex(expressionList, start, end);
+        double leftResult = evaluate(expressionList, start, operatorIndex - 1);
+        double rightResult = evaluate(expressionList, operatorIndex + 1, end);
+
+        char operator = expressionList.get(operatorIndex).charAt(0);
+        return applyOperator(leftResult, rightResult, operator);
+    }
+
+    private int findOperatorIndex(ArrayList<String> expressionList, int start, int end) {
+        int index = -1;
+        for (int i = start; i <= end; i++) {
+            if (isOperator(expressionList.get(i).charAt(0))) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    private double applyOperator(double left, double right, char operator) {
+        return switch (operator) {
+            case '+' -> left + right;
+            case '-' -> left - right;
+            case '*' -> left * right;
+            case '/' -> left / right;
+            default -> throw new IllegalArgumentException("Unknown operator: " + operator);
+        };
+    }
+
     private static boolean isOperator(char ch) {
-    return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '(' || ch == ')';
+        return ch == '+' || ch == '-' || ch == '*' || ch == '/';
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.evaluateMathExpression("3 + 5 * 2 - 8 / 4")); // Exemple de test
     }
 }
